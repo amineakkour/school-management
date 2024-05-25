a9afa436bca03a94136f2f7bcd7c12ce2b9c7e6c
amineakkour <amineakour6@gmail.com>
2024-05-25 19:24:28 +0100

    Add google-recaptcha to contact page

--- frontend/src/views/guest/Contact.jsx
+++ frontend/src/views/guest/Contact.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Link } from "react-router-dom";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const contactSchema = z.object({
   fullName: z.string().nonempty("Le nom complet est requis"),
@@ -13,11 +14,18 @@ const contactSchema = z.object({
 
 function Contact(props) {
   const [messageCounter, setMessageCounter] = useState(0);
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const [recaptchaValue, setRecaptchaValue] = useState(false);
+  const { register, handleSubmit, formState: { errors }, setError } = useForm({
     resolver: zodResolver(contactSchema),
   });
 
+  
+
   const onSubmit = (data) => {
+    if(!recaptchaValue) {
+      setError("recaptcha", {message: "Veuillez vérifier que vous etes pas un robot!"})
+      return ""
+    }
     console.log(data);
   };
 
@@ -26,8 +34,6 @@ function Contact(props) {
       <h1 className="text-2xl">Nous contacter</h1>
       <div className="text-gray-500 text-xs">Nous nous ferons un plaisir de répondre à vos questions le plus rapidement possible via votre numéro de téléphone.</div>
       <div className="text-gray-500 text-xs mb-8"><a href="/a-propos-de-nous#faq" className="underline">Visiter FAQ page.</a> Pour gagner du tmeps.</div>
-
-      {/* question fréquemment posée */}
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="my-2">
@@ -58,6 +64,11 @@ function Contact(props) {
           {errors.message && <div className="text-red-500 text-xs">{errors.message.message}</div>}
         </div>
 
+        <div className="my-2 max-w-md">
+          <ReCAPTCHA sitekey="6LeSCOgpAAAAALHK-0oGYg2U1ggQXBtCIQQ5s3xc" onChange={v => setRecaptchaValue(v)} />
+          {errors.recaptcha && <div className="text-red-500 text-xs">{errors?.recaptcha?.message}</div>}
+        </div>
+        
         <div className="my-5">
           <button type="submit" className="button-1">Enregistrer</button>
         </div>
