07b4958dfec91fad229e7e523b3864beddee672b
amineakkour <amineakour6@gmail.com>
2024-05-29 14:27:58 +0100

    Add users resources routes, logout and logout out from all devives

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\API\LoginController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +14,13 @@ Route::get('/user', function (Request $request) {
 Route::post("/student/login", [LoginController::class, "student_login"]);
 Route::post("/admin/login", [LoginController::class, "admin_login"]);
 Route::post("/teacher/login", [LoginController::class, "teacher_login"]);
+
+
+Route::post("/logout", [LoginController::class, "logout"]);
+Route::post("/logout_from_all_devices", [LoginController::class, "logout_from_all_devices"]);
+
+Route::resource("admin", AdminController::class);
+
+Route::resource("student", StudentController::class);
+
+Route::resource("teacher", TeacherController::class);
