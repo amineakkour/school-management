f50357aedd14396d3b071875b4fcfd4c3ab292e1
amineakkour <amineakour6@gmail.com>
2024-06-02 14:14:46 +0100

    fetch blogs from api

--- frontend/src/Layout/MainLayout.jsx
+++ frontend/src/Layout/MainLayout.jsx
@@ -4,10 +4,10 @@ import Footer from "../components/Footer";
 
 export default function MainLayout() {
   return (
-    <div className="max-w-screen-2xl mx-auto">
+    <div className="max-w-screen-2xl mx-auto relative">
       <Navbar />
   
-      <main className="max-w-6xl mt-10 md:mt-20 md:p-5 mx-2 md:mx-auto">
+      <main className="max-w-6xl mt-10 md:mt-20 md:p-5 mx-2 md:mx-auto min-h-screen">
         <Outlet />
       </main>
 
--- frontend/src/views/guest/Blog.jsx
+++ frontend/src/views/guest/Blog.jsx
@@ -1,29 +1,68 @@
-import HomeNavbar from "../../components/Navbar";
-import { useTheme } from "../../context/ThemeProvider";
+import { useEffect, useState } from "react";
+import { customAxios } from "../../api/customAxios";
+import { formatDate } from "../../functions/formatDate";
+import { Skeleton } from "@/components/ui/skeleton"
+import Alert from "../../components/Alert";
+
+function BlogSkelaton() {
+  return <div className={`bg-secondary rounded-lg p-4 mb-10 shadow-md pop-up`}>
+    <Skeleton className="bg-black/15 w-96 h-[20px]"></Skeleton>
+    <Skeleton className="bg-black/15 w-20 h-[15px] my-3"></Skeleton>
+    <Skeleton className="w-[600px] h-[350px] bg-black/15">
+    </Skeleton>
+    <div className="pt-4">
+      <Skeleton className="w-full h-[10px] bg-black/15 my-1"/>
+      <Skeleton className="w-[calc(100%-20px)] h-[10px] bg-black/15 my-1"/>
+      <Skeleton className="w-96 h-[10px] bg-black/15 my-1"/>
+    </div>
+  </div>;
+}
 
 function Card({title, createdAt, text, imgURL}) {
-  const { theme } = useTheme();
-
-  return <div className={`bg-secondary rounded-lg p-4 mb-10 shadow-md pop-up`}>
+  return <div className={`bg-secondary rounded-lg p-4 mb-10 shadow-md`}>
     <h3 className="text-xl">{title}</h3>
-    <div className="text-gray-400 mb-3">Créé à {createdAt}</div>
-    <div className="">
-      <img src={imgURL} alt={title} className="w-[600px] max-h-[350px] object-cover shadow-lg" />
+    <div className="text-gray-400 mb-3">Créé le {createdAt}</div>
+    <div className="w-[600px] h-[350px] bg-gray-200">
+      <img src={imgURL} alt={title} className="w-full h-full object-cover shadow-lg" />
     </div>
-    <p className="pt-2">{text}</p>
+    <p className="pt-4">{text}</p>
   </div>;
 }
 
 export default function Blog() {
+  const [blogs, setBlogs] = useState([]);
+  const [isFetched, setIsFetached] = useState(false);
+  const [alertText, setAlertText] = useState("")
+
+  async function fetchData() {
+    try{
+      const response = await customAxios('/blogs');
+      setBlogs(response.data)
+    }catch(error) {
+      setAlertText("Quelque chose s'est mal passé");
+      console.log(error)
+    }
+
+    setIsFetached(true)
+  } 
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className={`md:max-w-6xl p-5 mx-2 md:mx-auto`}>
+      {alertText && <Alert alertText={alertText} setAlertText={setAlertText} />}
       <div id="rain-bow"></div>
-      <h1 className="text-4xl py-5 pop-up">Blogs</h1>
+      <h1 className="text-4xl py-5">Blogs</h1>
 
-      <Card title="Qu'est-ce que le Lorem Ipsum?" createdAt={"2023-09-03"} text={"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles"} imgURL={"http://www.fsts.ac.ma/wp-content/uploads/2020/03/PC090174.jpg"} />
-      <Card title="Qu'est-ce que le Lorem Ipsum?" createdAt={"2023-09-03"} text={"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles"} imgURL={"https://www.jkeducate.co.uk/app/uploads/2022/08/classroom-2093744_1920.jpg.webp"} />  
-
+      {
+        isFetched
+        ?
+        blogs.map((blog, ind) => <Card key={ind} title={blog.title} createdAt={formatDate(blog.created_at)} text={blog.content} imgURL={`${import.meta.env.VITE_BACKEND_URL}${blog.photo_url}`} />)
+        :
+        Array.from(Array(5).keys()).map(i => <BlogSkelaton key={i} />)
+      }
   </div>
   )
 }
