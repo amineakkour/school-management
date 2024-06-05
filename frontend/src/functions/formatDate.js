d9a90106220e0746bfe94081a5e0584054d6b53a
amineakkour <amineakour6@gmail.com>
2024-06-05 11:08:46 +0100

    Add time to formatdate function

--- frontend/src/functions/formatDate.js
+++ frontend/src/functions/formatDate.js
@@ -1,5 +1,13 @@
 export function formatDate(dateString) {
-  const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  const options = { 
+    day: '2-digit', 
+    month: '2-digit', 
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  };
   const date = new Date(dateString);
   return date.toLocaleDateString('fr-FR', options);
 }
