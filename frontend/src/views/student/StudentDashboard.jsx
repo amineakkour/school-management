9f4f5f889d14d26cb04b58d9dd1daf9427bad38c
amineakkour <amineakour6@gmail.com>
2024-06-08 12:00:52 +0100

    Add logout

--- frontend/src/views/student/StudentDashboard.jsx
+++ frontend/src/views/student/StudentDashboard.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../redux/features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { customAxios } from '../../api/customAxios';
 
 function StudentDashboard(props) {
+  const { token } = useSelector(slice => slice.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  async function logoutCallBack() {
+    try{
+      const response = await customAxios.post('logout', {}, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log(response)
+    }catch(error){
+      console.error(error)
+    }
+
+    dispatch({type: "user/logout"});
+    navigate("/");
+  }
+
   return (
-    <div>
-      Hello Tudent !!
+    <div className='p-2'>
+      <ul>
+        <li onClick={logoutCallBack} className="underline text-blue-700 cursor-pointer">Log out</li>
+      </ul>
+      <div>Bonjour Etudiant</div>
     </div>
   );
 }
