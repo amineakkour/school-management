c615072ff89a775dff65ea4d1af050aa9229ab98
Amine Akkour <amineakour6@gmail.com>
2024-10-03 19:42:53 +0100

    check is api valid for each request

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,34 +13,30 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::post('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post("/student/login", [LoginController::class, "student_login"]);
-Route::post("/admin/login", [LoginController::class, "admin_login"]);
-Route::post("/teacher/login", [LoginController::class, "teacher_login"]);
 
-Route::post("/logout", [LoginController::class, "logout"])->middleware("auth:sanctum");
-Route::post("/logout_from_all_devices", [LoginController::class, "logout_from_all_devices"])->middleware("auth:sanctum");
 
 Route::resource("admins", AdminController::class);
-
 Route::resource("students", StudentController::class);
-
 Route::resource("teachers", TeacherController::class);
-
 Route::resource("payment-tranches", PaymenetTrancheController::class);
 
-Route::post("/payment-tranches-verify", [PaymenetTrancheController::class, "verify"]);
 
 Route::resource("blogs", BlogController::class);
-
 Route::resource("messages", MessageController::class);
 
 Route::get("students-counter", [StudentController::class, "counter"]);
 Route::get("get-student-id", [StudentController::class, "student_id"]);
-
 Route::get("teachers-counter", [TeacherController::class, "counter"]);
-
 Route::get("admins-counter", [AdminController::class, "counter"]);
+
+Route::post("/payment-tranches-verify", [PaymenetTrancheController::class, "verify"]);
+
+Route::post("/logout_from_all_devices", [LoginController::class, "logout_from_all_devices"])->middleware("auth:sanctum");
+Route::post("/logout", [LoginController::class, "logout"])->middleware("auth:sanctum");
+Route::post("/student/login", [LoginController::class, "student_login"]);
+Route::post("/admin/login", [LoginController::class, "admin_login"]);
+Route::post("/teacher/login", [LoginController::class, "teacher_login"]);
--- frontend/src/Layout/AdminLayout.jsx
+++ frontend/src/Layout/AdminLayout.jsx
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { switchToUrlBaseOnUserRole } from '../functions/switchToUrlBaseOnUserRole';
+import { customAxios } from '../api/customAxios';
+import Spinner from '../components/Spinner';
 
 function AdminLayout() {
   const user = useSelector(slice => slice.user);
   const [isLoaded, setIsLoaded] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(() => {
-    if(user.role !== "admin" || !user.token){
-      navigate(switchToUrlBaseOnUserRole('admin').loginPage, { replace: true })
+  // useEffect(() => {
+  //   if(user.role !== "admin" || !user.token){
+  //     navigate(switchToUrlBaseOnUserRole('admin').loginPage, { replace: true })
+  //   }
+
+  //   setIsLoaded(true)
+  // }, [])
+
+  async function checkApiTokenIsValid() {
+    const headers = {
+      Accept: 'application/json',
+      Authorization: `Bearer ${user.token}`
+    };
+    try{
+      const response = await customAxios.post("user", {}, { headers });
+      setIsLoaded(true);
+    }catch(error) {
+      console.error(error);
+      setIsLoaded(false);
+      navigate("/connexion/adminstrateur");
     }
-
-    setIsLoaded(true)
-  }, [])
+  }
   
-  if (!isLoaded) return ''
+  useEffect(() => checkApiTokenIsValid, [location]);
+  
+  if (!isLoaded) return <Spinner />
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Outlet />
