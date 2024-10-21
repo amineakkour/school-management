cb5dee0dc132d7d03ed281d82ed6db6396ea87b7
Amine Akkour <amineakour6@gmail.com>
2024-10-22 00:50:59 +0100

    add success message

--- frontend/src/views/guest/Contact.jsx
+++ frontend/src/views/guest/Contact.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 import { customAxios  } from '../../api/customAxios';
 
@@ -16,6 +15,7 @@ const contactSchema = z.object({
 function Contact(props) {
   const [messageCounter, setMessageCounter] = useState(0);
   const [recaptchaValue, setRecaptchaValue] = useState(false);
+  const [messageSent, setMessageSent] = useState(false);
   const { register, handleSubmit, formState: { errors }, setError, reset } = useForm({
     resolver: zodResolver(contactSchema),
   });
@@ -28,11 +28,14 @@ function Contact(props) {
 
     async function sendMessage() {
       var response = await customAxios.post('/messages', data);
-
-      console.log(response)
     }
 
-    sendMessage()
+    try{
+      sendMessage();
+      setMessageSent(true);
+    }catch(error) {
+      console.log(error)
+    }
     reset()
   };
 
@@ -46,44 +49,54 @@ function Contact(props) {
       <div className="text-gray-500 text-xs">Nous nous ferons un plaisir de répondre à vos questions le plus rapidement possible via votre numéro de téléphone.</div>
       <div className="text-gray-500 text-xs mb-8"><a href="/a-propos-de-nous#faq" className="underline">Visiter FAQ page.</a> Pour gagner du tmeps.</div>
 
+      {!messageSent && 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="my-2">
-          <label className="block mb-1 font-semibold">Nom complet</label>
-          <input type="text" {...register('fullName')} className="input-1 max-w-96 text-xs" />
-          {errors.fullName && <div className="text-red-500 text-xs">{errors.fullName.message}</div>}
-        </div>
+      <div className="my-2">
+        <label className="block mb-1 font-semibold">Nom complet</label>
+        <input type="text" {...register('fullName')} className="input-1 max-w-96 text-xs" />
+        {errors.fullName && <div className="text-red-500 text-xs">{errors.fullName.message}</div>}
+      </div>
 
-        <div className="my-2">
-          <label className="block mb-1 font-semibold">Numéro de téléphone</label>
-          <input type="text" {...register('phoneNumber')} className="input-1 max-w-96 text-xs" />
-          {errors.phoneNumber && <div className="text-red-500 text-xs">{errors.phoneNumber.message}</div>}
-        </div>
-        
-        <div className="my-2">
-          <label className="block mb-1 font-semibold">L'objet de message</label>
-          <input type="text" {...register('messageObject')} className="input-1 max-w-96 text-xs" />
-          {errors.messageObject && <div className="text-red-500 text-xs">{errors.messageObject.message}</div>}
-        </div>
+      <div className="my-2">
+        <label className="block mb-1 font-semibold">Numéro de téléphone</label>
+        <input type="text" {...register('phoneNumber')} className="input-1 max-w-96 text-xs" />
+        {errors.phoneNumber && <div className="text-red-500 text-xs">{errors.phoneNumber.message}</div>}
+      </div>
+      
+      <div className="my-2">
+        <label className="block mb-1 font-semibold">L'objet de message</label>
+        <input type="text" {...register('messageObject')} className="input-1 max-w-96 text-xs" />
+        {errors.messageObject && <div className="text-red-500 text-xs">{errors.messageObject.message}</div>}
+      </div>
 
-        <div className="my-2 max-w-md">
-          <label className="block mb-1 font-semibold">Message</label>
-          <div className="relative">
-            <textarea {...register('message')} className="input-1 text-xs" rows="5" onChange={(e) => setMessageCounter(e.target.value.length)} ></textarea>
+      <div className="my-2 max-w-md">
+        <label className="block mb-1 font-semibold">Message</label>
+        <div className="relative">
+          <textarea {...register('message')} className="input-1 text-xs" rows="5" onChange={(e) => setMessageCounter(e.target.value.length)} ></textarea>
 
-            <span className={`absolute bottom-2 right-2 text-xs ${(messageCounter < 50 || messageCounter > 250) ? "text-red-500" : "text-gray-500"}`}>{messageCounter}/250</span>
-          </div>
-          {errors.message && <div className="text-red-500 text-xs">{errors.message.message}</div>}
+          <span className={`absolute bottom-2 right-2 text-xs ${(messageCounter < 50 || messageCounter > 250) ? "text-red-500" : "text-gray-500"}`}>{messageCounter}/250</span>
         </div>
+        {errors.message && <div className="text-red-500 text-xs">{errors.message.message}</div>}
+      </div>
 
-        <div className="my-2">
-          <ReCAPTCHA sitekey="6LeSCOgpAAAAALHK-0oGYg2U1ggQXBtCIQQ5s3xc" onChange={v => setRecaptchaValue(v)} />
-          {errors.recaptcha && <div className="text-red-500 text-xs">{errors?.recaptcha?.message}</div>}
-        </div>
-        
-        <div className="my-5">
-          <button type="submit" className="button-1">Enregistrer</button>
-        </div>
-      </form>
+      <div className="my-2">
+        <ReCAPTCHA sitekey="6LeSCOgpAAAAALHK-0oGYg2U1ggQXBtCIQQ5s3xc" onChange={v => setRecaptchaValue(v)} />
+        {errors.recaptcha && <div className="text-red-500 text-xs">{errors?.recaptcha?.message}</div>}
+      </div>
+      
+      <div className="my-5">
+        <button type="submit" className="button-1">Enregistrer</button>
+      </div>
+    </form>
+    }
+
+    {messageSent && 
+      <div>
+        <h3 className='text-lg text-green-700'>Votre message a été envoyé avec succès.</h3>
+        <button className='button-1' onClick={() => setMessageSent(false)}>Ajouter un message</button>
+      
+      </div>
+    }
     </div>
   );
 }
